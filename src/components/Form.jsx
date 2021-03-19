da7c26d2de8ae521f0b49a475edcdfbdac845361
Rahul <1636rahul@gmail.com>
2021-03-20 02:09:59 +0530

    check image fixed

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -19,7 +19,7 @@ function TextArea(props, provided: DroppableProvided, snapshot: DroppableStateSn
     ]);
     const [isCompleted, setCompleted] = useState(false);
     const [isActive, setActive] = useState(false);
-    const [isDay, setDay] = useState(true);
+    const [isDay, setDay] = useState(false);
     const [viewport, setViewport] = useState(false);
     // const [Listlength, setLength] = useState(86.3)
     // const [characters, updateCharacters] = useState(task);
@@ -116,8 +116,10 @@ function TextArea(props, provided: DroppableProvided, snapshot: DroppableStateSn
 
     var backGroundDay = {backgroundColor: "hsl(0, 0%, 98%)", backgroundImage: viewport? "url(images/bg-mobile-light.jpg)": "url(images/bg-desktop-light.jpg)"};
     var backGroundNight = {backgroundColor: "hsl(235, 21%, 11%)", backgroundImage: viewport? "url(images/bg-mobile-dark.jpg)": "url(images/bg-desktop-dark.jpg)"};
-    var noteBoxShadow = {boxShadow: isDay? "rgba(0, 0, 0, 0.5) 0px 0px 1px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px" : "rgba(255, 255, 255, 0.5) 0px 0px 1px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px"};
-    var notesOuterShadow = {boxShadow: "rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.2) 0px 30px 60px -30px"};
+    // var noteBoxShadow = {boxShadow: isDay? "rgba(0, 0, 0, 0.5) 0px 0px 1px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px" : "rgba(255, 255, 255, 0.5) 0px 0px 1px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px"};
+    var noteBoxShadow = {};
+    var notesOuterShadow = {boxShadow: isDay ? "rgba(50, 50, 93, 0.40) 0px 50px 100px -20px, rgba(0, 0, 0, 0.5) 0px 30px 60px -30px": "rgba(0, 0, 0, 0.50) 0px 50px 100px -20px, rgba(0, 0, 0, 0.4) 0px 30px 60px -30px"};
+    // var notesOuterShadow = {};
     var toggleButtonsColor = {color: isDay? "hsl(235, 19%, 35%)": "hsl(233, 11%, 84%)"};
     // var sectionsHeight = {minHeight: "86.5rem", border: "1px solid red"};
     return (
@@ -137,9 +139,9 @@ function TextArea(props, provided: DroppableProvided, snapshot: DroppableStateSn
             <button className="submitNoteButton" onClick={() => {checkDuplicate(note); setnote({title: "", status: false});}} type="submit"></button>
             </div>
             <input style={{color: isDay? "hsl(235, 19%, 35%)": "hsl(0, 0%, 98%)"}} id="inputField" onChange={handleChange} type="text" value={note.title}  placeholder="Create a new todo.." autoComplete="off"/>
-
+            <svg id="input_check" onClick={() => {checkDuplicate(note); setnote({title: "", status: false});}} xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke={isDay? "hsl(235, 19%, 35%)": "hsl(233, 11%, 84%)"} strokeWidth="2" d="M1 4.304L3.696 7l6-6"/></svg>
             </div>
-            <div className="handle_outer_shadow">
+            <div style={notesOuterShadow} className="handle_outer_shadow">
 
             <DragDropContext onDragEnd={handleOnDragEnd}>
             <Droppable droppableId="characters">
@@ -179,19 +181,21 @@ function TextArea(props, provided: DroppableProvided, snapshot: DroppableStateSn
           </div>
 
 
-           <div id="todoStatus" style={{display: viewport? "flex": "none", backgroundColor: isDay? "white": "hsl(235, 24%, 19%)", boxShadow: noteBoxShadow.boxShadow}} className="buttons_container">
+           <div id="todoStatus" style={{display: viewport && task.length!==0 ? "flex": "none", backgroundColor: isDay? "white": "hsl(235, 24%, 19%)", boxShadow: noteBoxShadow.boxShadow}} className="buttons_container">
            <p> {(task.filter(item => item.status===isCompleted? true: false)).length} items {isCompleted? "completed": "left"}</p>
            <button type="button" onClick={clearCompleted}><p>Clear Completed</p></button>
            </div>
-           <div className="handle_outer_shadow">
+           <div style={notesOuterShadow} className="handle_outer_shadow">
+           <div style={{display: task.length===0 ? "none" : "flex"}} className="handle_outer_shadow">
            <div style={{backgroundColor: isDay? "white": "hsl(235, 24%, 19%)", boxShadow: noteBoxShadow.boxShadow}} className="buttons_container">
-            <p style={{display: viewport? "none": "flex"}}> {(task.filter(item => item.status===isCompleted? true: false)).length} items {isCompleted? "completed": "left"}</p>
-            <div style={{noteBoxShadow}} className="toggleButtons">
+            <p style={{display: viewport ? "none": "flex"}}> {(task.filter(item => item.status===isCompleted? true: false)).length} items {isCompleted? "completed": "left"}</p>
+            <div className="toggleButtons">
             <button type="button" onClick={renderCompleted}><p style={{color: (isActive===false&&isCompleted===false&&task.length!==0)? "hsl(220, 98%, 61%)": toggleButtonsColor.color}}>All</p></button>
             <button type="button" onClick={handleActive}><p style={{color: (isActive&&task.length!==0)? "hsl(220, 98%, 61%)": toggleButtonsColor.color}}>Active</p></button>
             <button type="button" onClick={handleCompleted}><p style={{color: (isCompleted&&task.length!==0)? "hsl(220, 98%, 61%)": toggleButtonsColor.color}}>Completed</p></button>
             </div>
             <button style={{display: viewport? "none": "flex"}} type="button" onClick={clearCompleted}><p>Clear Completed</p></button>
+            </div>
             </div>
             </div>
            </form>
--- src/components/note.jsx
+++ src/components/note.jsx
@@ -8,13 +8,13 @@ function Note(props) {
     var dayFont = {color: props.status === false? "hsl(235, 19%, 35%)": "hsl(233, 11%, 84%)"};
     var NightFont = {color: props.status === false? "hsl(233, 11%, 84%)": "hsl(235, 19%, 35%)"};
     var noteBoxShadow = {boxShadow: props.isDay? "rgba(0, 0, 0, 0.5) 0px 0px 1px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px" : "rgba(255, 255, 255, 0.5) 0px 0px 1px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px"}
-
+    // var noteBorder = {border: "0.001px solid white"};
 
     return (
 
 
 
-        <div style={{display: (props.isCompleted && props.status===false) || (props.isActive && props.status===true) ? "none": "flex", backgroundColor: props.isDay? "hsl(0, 0%, 98%)": "hsl(235, 24%, 19%)", boxShadow: noteBoxShadow.boxShadow, border: "1px solid blackk "}} className="note_container">
+        <div style={{display: (props.isCompleted && props.status===false) || (props.isActive && props.status===true) ? "none": "flex", backgroundColor: props.isDay? "hsl(0, 0%, 98%)": "hsl(235, 24%, 19%)", boxShadow: noteBoxShadow.boxShadow}} className="note_container">
           <div className="check_container">
           <div style={{background: props.status? "linear-gradient(hsl(192, 100%, 67%) , hsl(280, 87%, 65%))": "transparent"}} onClick={() => {props.handleChecks()}} id="circle">
           <svg style={{display: props.status? "flex": "none"}} xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#fff" strokeWidth="2" d="M1 4.304L3.696 7l6-6"/></svg>
